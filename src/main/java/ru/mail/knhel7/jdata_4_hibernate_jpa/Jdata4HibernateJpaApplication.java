0e425f66954f7f8449cfd6ac45fc05c1526138a4
kievskiy <knhel7@mail.ru>
2024-03-11 23:39:12 +0300

    Init

---
+++ src/main/java/ru/mail/knhel7/jdata_4_hibernate_jpa/Jdata4HibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.mail.knhel7.jdata_4_hibernate_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdata4HibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdata4HibernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mail/knhel7/jdata_4_hibernate_jpa/Jdata4HibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mail.knhel7.jdata_4_hibernate_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdata4HibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
